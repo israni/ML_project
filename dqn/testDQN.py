f3dc27956e84edfe83f373156d21cec6fba5afd8
girikum <girikum@umich.edu>
2018-12-04 03:10:42 -0500

    testingDQN code

--- dqn/testDQN.py
+++ dqn/testDQN.py
@@ -2,19 +2,49 @@ import gym
 from helperfns import *
 from architecture import *
 import os
+import sys
 import tensorflow as tf
 import numpy as np
 import matplotlib as plt
 
+path = "./models"
+
+def plotData():
+	csvFile = open(path+"/trainingData.csv", "r")
+
+	csvFile.close()
+
 if __name__=='__main__':
 	env = gym.make('BreakoutDeterministic-v4')
-	frame = env.reset()
-	env.render()
+	
+	tf.reset_default_graph()
+	mainQN = networkArchitecture()
+	targetQN = networkArchitecture()
 
-	isItTimeToReset = False
-	while not isItTimeToReset:
-		frame, reward, isItTimeToReset, _ = env.step(env.action_space.sample())
+	initOp = tf.global_variables_initializer()
+	
+	episodeNumber = 400
+	
+	filePath = path+'/model'+str(episodeNumber)+'.ckpt'
+
+	saver =tf.train.Saver()
+	isTimeToReset = False
+
+	plotData()
+	with tf.Session() as sess:
+		sess.run(initOp)
+		print('Loading Model...')
+		saver.restore(sess,filePath)
+		state = env.reset()
+		state = phi(state) #Process states
 		env.render()
+		while not isTimeToReset:
+			action = sess.run(mainQN.predict, feed_dict = {mainQN.ipFrames:[state]})[0]
+			state, reward, isTimeToReset = performAction(env,action)
+			state = phi(state)
+			print(reward,isTimeToReset)
+			env.render()
+		
 
 	env.close()
 	
