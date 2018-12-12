67c8f283aec97cb2e565692f17167facf9045132
Sagar Israni <israni@umich.edu>
2018-12-12 18:42:26 -0500

    Network defined

---
+++ atari_dqn.py
@@ -0,0 +1,19 @@
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Flatten, Convolution2D, Permute
+
+num_actions = 4
+
+
+model = Sequential()
+model.add(Convolution2D(32, (8, 8), strides=(4, 4)))
+model.add(Activation('relu'))
+model.add(Convolution2D(64, (4, 4), strides=(2, 2)))
+model.add(Activation('relu'))
+model.add(Convolution2D(64, (3, 3), strides=(1, 1)))
+model.add(Activation('relu'))
+model.add(Flatten())
+model.add(Dense(512))
+model.add(Activation('relu'))
+model.add(Dense(num_actions))
+model.add(Activation('linear'))
+print(model.summary())
