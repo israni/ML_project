6871668b1b5db0f93955e23a53cbf4129522773d
Sagar Israni <israni@umich.edu>
2018-12-12 19:04:46 -0500

    Input shape added

--- atari_dqn.py
+++ atari_dqn.py
@@ -3,6 +3,11 @@ from keras.layers import Dense, Activation, Flatten, Convolution2D, Permute
 
 num_actions = 4
 
+INPUT_SHAPE = (84, 84)
+WINDOW_LENGTH = 4
+
+input_shape = INPUT_SHAPE + (WINDOW_LENGTH,)
+print(input_shape)
 
 model = Sequential()
 model.add(Convolution2D(32, (8, 8), strides=(4, 4)))
