aa43f22933e6af1d809588578d799b6fd64bdff9
Sagar Israni <israni@umich.edu>
2018-12-14 11:18:34 -0500

    videos and graphs generated

--- DQN/log_play.py
+++ DQN/log_play.py
@@ -4,49 +4,47 @@ import json
 import matplotlib.pyplot as plt
 
 
-def log_play(filename1, filename2, figsize=None, output=None):
+def log_play(filename1, filename2, filename3):
     
-
+	# file1 - data1
     with open(filename1, 'r') as f1:
         data1 = json.load(f1)
-    if 'episode' not in data1:
-        raise ValueError('Log file "{}" does not contain the "episode" key.'.format(filename))
     nb_steps1 = data1['nb_steps']
 
+    # file2 - data2
     with open(filename2, 'r') as f2:
         data2 = json.load(f2)
-    if 'episode' not in data2:
-        raise ValueError('Log file "{}" does not contain the "episode" key.'.format(filename))
     nb_steps2 = data2['nb_steps']
 
-    # Get value keys. The x axis is shared and is the number of episodes.
-    keys1 = sorted(list(set(data1.keys()).difference(set(['nb_steps','mean_absolute_error','mean_eps','nb_episode_steps']))))
-    keys2 = sorted(list(set(data2.keys()).difference(set(['nb_steps','mean_absolute_error','mean_eps','nb_episode_steps']))))
+    # file3 - data3
+    with open(filename3, 'r') as f3:
+        data3 = json.load(f3)
+    nb_steps3 = data3['nb_steps']
 
-    if figsize is None:
-        figsize = (150., 5.)
+    # Get value keys. The x axis is shared and is the number of episodes.
+    keys = sorted(list(set(data1.keys()).difference(set(['nb_steps','mean_absolute_error','mean_eps','nb_episode_steps']))))
     
-    for i in range(0,len(keys1)):
-    	plt.figure(figsize=figsize)
-    	plt.plot(nb_steps1,data1[keys1[i]])
-    	plt.plot(nb_steps1,data2[keys2[i]])
+    figsize = (150., 5.)
+    
+
+    for i in range(0,len(keys)):
+    	#plt.figure(figsize=figsize)
+    	fig, ax = plt.subplots(figsize = figsize)
+    	ax.plot(nb_steps1[0:4676],data1[keys[i]][0:4676],label ='seed 111')
+    	ax.plot(nb_steps2,data2[keys[i]],label = 'seed 555')
+    	ax.plot(nb_steps3[0:4676],data3[keys[i]][0:4676],label = 'seed 999')
+    	ax.legend()
     	plt.xlabel('number of steps')
-    	plt.ylabel(keys1[i])
+    	plt.ylabel(keys[i])
     
-    if output is None:
-        plt.show()
-    else:
-        plt.savefig(output)
+    plt.show()
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('filename1', type=str, help='The filename of the JSON log generated during training.')
-parser.add_argument('filename2', type=str, help='The filename of the JSON log generated during training.')
-#parser.add_argument('filename3', type=str, help='The filename of the JSON log generated during training.')
-parser.add_argument('--output', type=str, default=None, help='The output file. If not specified, the log will only be displayed.')
-parser.add_argument('--figsize', nargs=2, type=float, default=None, help='The size of the figure in `width height` format specified in points.')
+parser.add_argument('filename1', type=str)
+parser.add_argument('filename2', type=str)
+parser.add_argument('filename3', type=str)
 args = parser.parse_args()
 
-# You can use visualize_log to easily view the stats that were recorded during training. Simply
-# provide the filename of the `FileLogger` that was used in `FileLogger`.
-log_play(args.filename1,args.filename2, output=args.output, figsize=args.figsize)
+# Call plotting function
+log_play(args.filename1,args.filename2,args.filename3)
